d38a0acd5cc4dc1c4415d5ba749655163dce9af3
rz8et <72686599+rzet8@users.noreply.github.com>
2020-12-19 11:26:22 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import eventlet
+eventlet.monkey_patch()
+
+import flask_socketio
+
+from flask_socketio import SocketIO, send, emit
+
+from flask import Flask, \
+    redirect, render_template, request, session
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+@socketio.on('message')
+def get_data(data):
+    if "connected" in data:
+        emit('bar', data, broadcast=True)
+        print("[USER] "+data)
+    else:
+        send(data, broadcast=True)
+
+users = []
+
+@app.route("/", methods=['POST', 'GET'])
+def index_page():
+    try:
+        aUser = session['username']
+        return render_template("index.html", username=aUser, title="Chat")
+    except:
+        rUser = request.args.get('username')
+        if rUser:
+            if rUser not in users: 
+                session['username'] = rUser
+                users.append(rUser)
+            return redirect("/")
+
+        else:
+            return render_template("index.html", username=None, title="Auth")
+
+@app.route("/exit")
+def exit_page():
+    try:
+        user = session['username']
+        del session['username']
+        users.remove(user)
+    except:
+        pass
+
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.secret_key = 'efewhfuyy2138423ijkr53249085-90rvg8e-3j42'
+    app.config['SECRET_KEY'] = 'efewhfuyy2138423ijkr53249085-90rvg8e-3j423242'
+    print("[CONSOLE] Server run")
+    socketio.run(app)
+
+
+
---
+++ config.py
@@ -0,0 +1,8 @@
+#configuration file
+
+ADMIN_PASSWORD = "admin"
+ADMIN_TOKEN = "GCfsIVk3e1}BWs%k*TB6DQw~FlJB0aHL9sHqkEtas6N11{Ky~23}W|A%~vdF@~r@"
+
+SECRET_SESSION_KEY = "FuEjHH$75rAE6bxbKzt}N{NGg1At|yoi|DvhbKnGsCvc0XN~2uw8vsxrV$|IPMBf"
+
+TITLE = "SSHOP"
